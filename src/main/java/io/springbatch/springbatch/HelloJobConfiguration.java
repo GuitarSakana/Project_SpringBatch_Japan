862966ac91159382a4f5c38486211c9eed98deda
Guitarfish <ayrcap@gmail.com>
2024-11-17 18:26:33 +0900

    Spring Batch Project for Japan

--- src/main/java/io/springbatch/springbatch/HelloJobConfiguration.java
+++ src/main/java/io/springbatch/springbatch/HelloJobConfiguration.java
@@ -1,7 +1,59 @@
 package io.springbatch.springbatch;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
+@Slf4j
 @Configuration
 public class HelloJobConfiguration {
+    //Spring Boot 3.0이상 버전 부터는 JobBuilderFactory랑 StepBuilderFactory가 필요가 없다.
+
+    @Bean
+    public Job helloJob(JobRepository jobRepository, Step helloStep, Step helloStep2) {
+        return new JobBuilder("helloJob", jobRepository)
+                .start(helloStep)
+                .next(helloStep2)
+                .build();
+    }
+    @Bean
+    public Step helloStep(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager){
+        return new StepBuilder("helloStep", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("====================================");
+                    System.out.println(" hello Spring Batch!! ");
+                    System.out.println("====================================");
+                    return RepeatStatus.FINISHED;
+                }
+                , platformTransactionManager)
+                .build();
+    }
+    @Bean
+    public Step helloStep2(JobRepository jobRepository,PlatformTransactionManager platformTransactionManager){
+        return new StepBuilder("helloStep2",jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("====================================");
+                    System.out.println(" hello Spring Batch2 was Excute!! ");
+                    System.out.println("====================================");
+                    return RepeatStatus.FINISHED;
+                }, platformTransactionManager)
+                .build();
+    }
+
+//    @Bean
+//    public Tasklet testTasklet(){
+//        return ((contribution, chunkContext) -> {
+//            log.info(">>>>> This is Step1");
+//            return RepeatStatus.FINISHED;
+//        });
+//    }
+
 }
--- src/main/java/io/springbatch/springbatch/SpringBatchApplication.java
+++ src/main/java/io/springbatch/springbatch/SpringBatchApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableBatchProcessing //Spring Batch를 돌리기 위한 어노테이션(선언)
+//@EnableBatchProcessing //Spring Batch를 돌리기 위한 어노테이션(선언) SpringBoot 3.0이상 부터는 사용x
 public class SpringBatchApplication {
 
     public static void main(String[] args) {
