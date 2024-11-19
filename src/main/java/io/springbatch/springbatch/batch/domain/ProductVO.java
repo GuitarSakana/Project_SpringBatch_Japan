29c701efdd4b9b0b0fb9ad0fef50bc1c4b8b96c7
Guitarfish <ayrcap@gmail.com>
2024-11-19 22:38:55 +0900

    Spring Batch in Japan_ProductVO&Product

--- src/main/java/io/springbatch/springbatch/batch/domain/ProductVO.java
+++ src/main/java/io/springbatch/springbatch/batch/domain/ProductVO.java
@@ -1,15 +1,19 @@
 package io.springbatch.springbatch.batch.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductVO {
 
-    @Id
-    @GeneratedValue
     private Long id;
+    private String name;
+    private int price;
+    private String type;
+
 }
