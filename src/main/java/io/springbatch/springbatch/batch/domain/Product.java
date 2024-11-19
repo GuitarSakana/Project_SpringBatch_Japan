7478c6255c564025f6b67542f9e44b49836adc8d
Guitarfish <ayrcap@gmail.com>
2024-11-19 23:56:19 +0900

    Spring Batch in Japan_fileJob clear

--- src/main/java/io/springbatch/springbatch/batch/domain/Product.java
+++ src/main/java/io/springbatch/springbatch/batch/domain/Product.java
@@ -2,10 +2,12 @@ package io.springbatch.springbatch.batch.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table
 public class Product {
 
     @Id
