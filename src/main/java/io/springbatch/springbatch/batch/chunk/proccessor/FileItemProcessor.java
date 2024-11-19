dd3c65aa7cc738c2df1da603284a72d6a35e325f
Guitarfish <ayrcap@gmail.com>
2024-11-19 23:30:53 +0900

    Spring Batch in Japan_Setting ModelMapper in pop.xml

--- src/main/java/io/springbatch/springbatch/batch/chunk/proccessor/FileItemProcessor.java
+++ src/main/java/io/springbatch/springbatch/batch/chunk/proccessor/FileItemProcessor.java
@@ -2,12 +2,16 @@ package io.springbatch.springbatch.batch.chunk.proccessor;
 
 import io.springbatch.springbatch.batch.domain.Product;
 import io.springbatch.springbatch.batch.domain.ProductVO;
+import org.modelmapper.ModelMapper;
 import org.springframework.batch.item.ItemProcessor;
 
 public class FileItemProcessor implements ItemProcessor<ProductVO, Product> {
 
     @Override
     public Product process(ProductVO item) throws Exception {
+
+        ModelMapper
+
         return null;
     }
 }
