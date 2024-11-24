bd37256f99671bc49797d1b97cff2e973675a1b2
Guitarfish <ayrcap@gmail.com>
2024-11-24 17:30:35 +0900

    Spring Batch in Japan_create ItemProcessor Method

---
+++ src/main/java/io/springbatch/springbatch/batch/classifier/ProcessorClassifier.java
@@ -0,0 +1,28 @@
+package io.springbatch.springbatch.batch.classifier;
+
+
+import io.springbatch.springbatch.batch.domain.ApiRequestVO;
+import io.springbatch.springbatch.batch.domain.ProductVO;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.classify.Classifier;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProcessorClassifier<C, T> implements Classifier<C,T> {
+
+    private Map<String, ItemProcessor<ProductVO, ApiRequestVO>> processorMap
+            = new HashMap<>();
+
+    @Override
+    public T classify(C classifiable) {
+
+        return (T)processorMap.get(((ProductVO)classifiable).getType());
+
+    }
+
+    public void setProcessorMap(Map<String, ItemProcessor<ProductVO, ApiRequestVO>> processorMap) {
+        this.processorMap = processorMap;
+    }
+}
+
